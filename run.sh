1b4e4108901fc736478f0f23e0a82d1d31347ac4
Quang Vinh <voquangvinh1206@gmail.com>
2026-04-28 11:04:39 +0700

    add loss to huggingfaces

--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
-#SBATCH --job-name=train_ot_qa
-#SBATCH -p kisski-h100
-#SBATCH --mem=128G
-#SBATCH -c 8
-#SBATCH -G H100:1
-#SBATCH -t 48:00:00
-#SBATCH --output=slurm-%x-%j.out
-#SBATCH --error=slurm-%x-%j.err
-#SBATCH --mail-type ALL
-#SBATCH --constraint=inet
+!/bin/bash
+SBATCH --job-name=train_ot_qa
+SBATCH -p kisski-h100
+SBATCH --mem=128G
+SBATCH -c 8
+SBATCH -G H100:1
+SBATCH -t 48:00:00
+SBATCH --output=slurm-%x-%j.out
+SBATCH --error=slurm-%x-%j.err
+SBATCH --mail-type ALL
+SBATCH --constraint=inet
 
 BASE=/projects/extern/kisski/kisski-imm/dir.project/CROSS-LINGUAL-QA-WITH-OPTIMAL-TRANSPORT-AND-GRAPH-BASED-REASONING-ALIGNMENT-
 
-# Thiết lập các biến môi trường quan trọng
 export PYTHONUNBUFFERED=1
 export TOKENIZERS_PARALLELISM=false
 export HF_HOME=$BASE/.cache/huggingface
+
+export HF_TOKEN=$(cat $BASE/.hf_token)
 
 source $BASE/venv/bin/activate
 
